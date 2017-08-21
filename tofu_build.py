8e6f9c667a29c451271e95f0570b96da1af2f80a
tofurama3000 <tofurama3000@hotmail.com>
2017-08-21 10:30:04 -0600

    Added command to build man pages

--- tofu_build.py
+++ tofu_build.py
@@ -19,7 +19,7 @@ def load_proj():
     if os.path.exists(yaml_file):
         with open(yaml_file) as proj_file:
             proj_conf = yaml.load(proj_file)
-            if 'type' not in proj_conf or proj_conf['type'] != 'tofu_build':
+            if 'type' not in proj_conf or proj_conf['type'].strip() != 'tofu_build':
                 raise EnvironmentError("Unknown project type!")
     else:
         raise EnvironmentError("Unable to find project!")
@@ -261,12 +261,15 @@ def build(production, verbose):
     click.echo()
 
 
-def print_info(info, tabs=0):
+def print_info(info, tabs=0, print_func=None, tab="\t"):
     def prnt(statement):
         tbs = ""
         for i in range(tabs):
-            tbs += "\t"
-        click.echo("%s%s" % (tbs, statement))
+            tbs += tab
+        if print_func is None:
+            click.echo("%s%s" % (tbs, statement))
+        else:
+            print_func("%s%s" % (tbs, statement))
 
     if isinstance(info, dict):
         if 'desc' not in info:
@@ -274,18 +277,49 @@ def print_info(info, tabs=0):
             keys.sort()
             for k in keys:
                 prnt(k)
-                print_info(info[k], tabs+1)
+                print_info(info[k], tabs+1, print_func, tab)
         else:
-            print_info("Description: %s " % info['desc'], tabs)
+            print_info(
+                "Description: %s " % info['desc'],
+                tabs,
+                print_func,
+                tab
+            )
             if 'default' in info:
-                print_info("Default value: %s" % str(info['default']), tabs)
+                print_info(
+                    "Default value: %s" % str(info['default']),
+                    tabs,
+                    print_func,
+                    tab
+                )
             else:
-                print_info("[ REQUIRED ]", tabs)
+                print_info(
+                    "[ REQUIRED ]",
+                    tabs,
+                    print_func,
+                    tab
+                )
     elif isinstance(info, (list, tuple)):
         for v in info:
-            print_info(v, tabs + 1)
+            print_info(
+                v,
+                tabs + 1,
+                print_func,
+                tab
+            )
     else:
         prnt(info)
+
+
+class InternalStringAppend:
+    def __init__(self):
+        self.str = ""
+
+    def append(self, str):
+        self.str += str + "\n"
+
+    def get(self):
+        return self.str
 
 
 @click.command()
@@ -294,9 +328,56 @@ def instructions():
     print_info(instructions)
 
 
+@click.command()
+def build_man():
+    str = InternalStringAppend()
+    print_info(proj_conf.proj_instructions(), print_func=str.append, tab="    ")
+    man_page = """
+." Manpage for tofu_build'
+.TH tofu_build 1 "21 Aug 2017" "v0.1" "tofu_build man page"
+.SH NAME
+tofu_build \\- Runs the TofuSoftware build system
+.SH SYNOPSIS
+tofu_build [OPTIONS] ACTION
+.SH DESCRIPTION
+tofu_build is a build system built around CMake and Docker. It creates two docker containers, one for building the application and one for running it. It manages the project configuration for both containers, allowing a centralized location for managing the configuration for two devices. It also generates CMakeLists.txt for use in the build Docker container. Unit tests are ran in both containers. The script also manages the build process by building and running the Docker images and checking at each step to make sure everything succeeds before moving on to the next step. The script also handles clean-up of docker containers and generated files (docker images are kept for faster future builds, those need to be cleaned manually). Currently only C++ is supported, but there are plans for other languages to be added in the future.
+.SH ACTIONS
+Below is a list of actions for the application:
+    - clean
+        The clean command will clean generated files and attempt to clean the build folder. However, due to permissions with mounted volumes, you may need to manually remove the build folder as root.
+    - config
+        This will create just the configuration files. It is called automatically by build.
+        
+        Options:
+            --legacy - If present, tofu_bulid will output the legacy config files where you run the build.sh file manually to start the bulid process. This has been deprecated and will be removed by version 1.0
+    - build
+        This builds the project located in the current working directory.
+        
+        Options:
+            --production - If present, tofu_bulid will build and test the production Docker environment, otherwise it will only test the build docker image
+            --verbose - If present, will enable verbose mode
+    - instructions
+        Will print out project file specifications
+.SH PROJECT FILE
+The project file for tofu_build has the YAML format. It controls steps used when configuring the Docker images and creating the CMakeLists.txt file, including what packages will be installed, commands to run, libraries to link, etc. The format is outlined below
+
+""" + str.get() + """
+
+.SH SEE ALSO
+For further information, see the README.md file
+.SH BUGS
+Bugs and issues will be tracked at https://github.com/tofurama3000/tofu_build/issues
+.SH AUTHOR
+Matthew Tolman (aka tofurama3000) https://github.com/tofurama3000
+"""
+    with open('tofu_build', 'w') as man_page_fd:
+        man_page_fd.write(man_page)
+
+
 cli.add_command(clean)
 cli.add_command(config)
 cli.add_command(build)
 cli.add_command(instructions)
+cli.add_command(build_man)
 
 cli()
